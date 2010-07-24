b86a4db8c069cab5770ba6661ecd913c4bb1d9c5
Eloy Duran <eloy.de.enige@gmail.com>
2010-07-25 00:57:54 +0200

    Small cleanup

--- lib/irb/cocoa/view_controller.rb
+++ lib/irb/cocoa/view_controller.rb
@@ -20,45 +20,33 @@ class IRBViewController < NSViewController
   
   def outlineView(outlineView, numberOfChildrenOfItem: item)
     if item == nil
-      # p "number of items: #{@results.size + 1}"
+      # add one extra which is the input row
       @results.size + 1
     end
   end
   
   def outlineView(outlineView, isItemExpandable: item)
-    # p outlineView, item
     false
   end
   
   def outlineView(outlineView, child: index, ofItem: item)
-    # p outlineView, child, item
     if item == nil
-      # p "asked for child at #{index}"
       if index == @results.size
-        # p "INPUT!"
         :input
       else
-        # p @results[index]
         @results[index]
       end
     end
   end
   
   def outlineView(outlineView, objectValueForTableColumn: column, byItem: item)
-    # p outlineView, column, item
-    # p item
-    if item == nil
-      nil
-    elsif item == :input
-      # p "asked for input"
+    if item == :input
       if column.identifier == "prompt"
         "irb>"
       else
         ""
       end
-    else
-      # p "asked for object value for #{item.inspect}"
-      # p column.identifier
+    elsif item
       item[column.identifier]
     end
   end
