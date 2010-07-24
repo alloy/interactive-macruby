8b353a6447752276eaf907e92f2041628dc315df
Eloy Duran <eloy.de.enige@gmail.com>
2010-07-24 18:09:57 +0200

    Add app skeleton

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Interactive MacRuby
+//
+//  Created by Eloy Duran on 24-07-10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
