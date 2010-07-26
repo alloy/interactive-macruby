d5365910e6f6651a724156b9e6702fe097fd0f55
Eloy Duran <eloy.de.enige@gmail.com>
2010-07-26 21:07:40 +0200

    Add spec for BasicNode

---
+++ spec/node_spec.rb
@@ -0,0 +1,36 @@
+framework "AppKit"
+require "mspec"
+$:.unshift File.expand_path("../../lib", __FILE__)
+require "irb/cocoa/node"
+
+describe "BasicNode" do
+  extend IRB::Cocoa
+
+  before do
+    @node = BasicNode.alloc.initWithStringRepresentation("42")
+  end
+
+  it "returns an attributed string as prefix" do
+    @node.prefix.is_a?(NSAttributedString).should == true
+  end
+
+  it "returns an empty prefix by default" do
+    @node.prefix.string.should == ""
+  end
+
+  it "returns the string repre" do
+    @node.stringRepresentation.is_a?(NSAttributedString).should == true
+  end
+
+  it "returns the string representation" do
+    @node.stringRepresentation.string.should == "42"
+  end
+
+  it "is not expandable" do
+    @node.expandable?.should == false
+  end
+
+  it "returns an empty array as children" do
+    @node.children.should == []
+  end
+end
