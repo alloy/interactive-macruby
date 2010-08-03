c7447afe8c6052c2421cd1662fdc2348c69d2eda
Eloy Duran <eloy.de.enige@gmail.com>
2010-08-03 23:15:12 +0200

    Make command+n open a new top level object session window

--- application_delegate.rb
+++ application_delegate.rb
@@ -2,6 +2,10 @@ TOPLEVEL_OBJECT = self
 
 class ApplicationDelegate
   def applicationWillFinishLaunching(notification)
+    newTopLevelObjectSession(nil)
+  end
+
+  def newTopLevelObjectSession(sender)
     irb(TOPLEVEL_OBJECT, TOPLEVEL_BINDING.dup)
   end
 end
